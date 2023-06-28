2093fdf9d7f05f38aa552ec1b80ec3599a2a4f75
Vyctoria Karina <vyctoriak@gmail.com>
2023-06-28 21:10:09 +0200

    route on the links, and stylization when icon is active

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -26,5 +26,9 @@ export const HeaderContainer = styled.header`
     &:hover {
       border-bottom: 3px solid ${(props) => props.theme['green-500']};
     }
+
+    &.active {
+      color: ${(props) => props.theme['green-500']};
+    }
   }
 `
