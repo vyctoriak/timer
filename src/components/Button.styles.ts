b61a05bc61d8b8b4adb598c7462b480bd3575ad0
Vyctoria Karina <vyctoriak@gmail.com>
2023-06-28 20:21:46 +0200

    fixing lint

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -1,9 +1,9 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
-export type ButtonVariant = "primary" | "secondary" | "danger" | "success";
+export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success'
 
 interface ButtonContainerProps {
-  variant: ButtonVariant;
+  variant: ButtonVariant
 }
 
 export const ButtonContainer = styled.button<ButtonContainerProps>`
@@ -14,6 +14,6 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
   margin: 0 1rem;
   font-weight: bold;
   font-size: 1rem;
-  background-color: ${(props) => props.theme["green-500"]};
+  background-color: ${(props) => props.theme['green-500']};
   color: ${(props) => props.theme.white};
-`;
+`
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,4 +1,4 @@
-import { ButtonContainer, ButtonVariant } from "./Button.styles";
+import { ButtonContainer, ButtonVariant } from './Button.styles'
 
 interface ButtonProps {
   variant?: ButtonVariant
