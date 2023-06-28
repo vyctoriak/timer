6b194d7390a5360351a093f2731258511bf3da61
Vyctoria Karina <vyctoriak@gmail.com>
2023-06-28 20:03:22 +0200

    styled components, global theme, types, initial configuration

---
+++ src/components/Button.styles.ts
@@ -0,0 +1,19 @@
+import styled, { css } from "styled-components";
+
+export type ButtonVariant = "primary" | "secondary" | "danger" | "success";
+
+interface ButtonContainerProps {
+  variant: ButtonVariant;
+}
+
+export const ButtonContainer = styled.button<ButtonContainerProps>`
+  width: 100px;
+  height: 40px;
+  cursor: pointer;
+  border: none;
+  margin: 0 1rem;
+  font-weight: bold;
+  font-size: 1rem;
+  background-color: ${(props) => props.theme.primary};
+  color: ${(props) => props.theme.white};
+`;
