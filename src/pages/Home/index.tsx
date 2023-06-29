d80c849a6b97a04edd34a74c5d5a9b4a8f029e96
Vyctoria Karina <vyctoriak@gmail.com>
2023-06-29 11:53:23 +0200

    datalist with project/tasks name

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -15,7 +15,17 @@ export function Home() {
       <form action="#">
         <FormContainer>
           <label htmlFor="task">I will work in</label>
-          <TaskInput id="task" placeholder="give your task a name" />
+          <TaskInput
+            id="task"
+            list="task-suggestions"
+            placeholder="give your task a name"
+          />
+
+          <datalist id="task-suggestions">
+            <option value="First Project" />
+            <option value="Second Project" />
+            <option value="Third Project" />
+          </datalist>
 
           <label htmlFor="minutesAmount">during</label>
           <MinutesAmountInput
